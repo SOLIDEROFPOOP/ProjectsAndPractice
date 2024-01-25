619d40f2ba5c838dd92cb10d61f950aaa4460274
Kapparov Murat <136073953+SOLIDEROFPOOP@users.noreply.github.com>
2024-01-26 05:19:06 +0600

    FindMinimumInRotatedArray, SearchInRotatedSortedArray

---
+++ LeetCodeProblems/src/SearchInRotatedSortedArray.java
@@ -0,0 +1,31 @@
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; // Found the target, return its index
+            }
+
+            if (nums[left] <= nums[mid]) {
+                // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; // Target is in the left sorted half
+                } else {
+                    left = mid + 1; // Target is in the right half
+                }
+            } else {
+                // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; // Target is in the right sorted half
+                } else {
+                    right = mid - 1; // Target is in the left half
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+}
