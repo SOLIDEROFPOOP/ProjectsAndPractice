42f974c2a2d981f99d172264252370ac9febcdb2
Kapparov Murat <136073953+SOLIDEROFPOOP@users.noreply.github.com>
2024-01-28 13:14:02 +0600

    Sliding window algorithms started

--- LeetCodeProblems/src/BestTimeToBuyAndSellStock.java
+++ LeetCodeProblems/src/BestTimeToBuyAndSellStock.java
@@ -1,11 +1,11 @@
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] profit){
-        int minPrice = profit[0];
         int maxProfit = 0;
+        int buy = profit[0];
+        int sell;
         for (int i = 0; i < profit.length; i++){
-            minPrice = Math.min(minPrice , profit[i]);
-            int potentialProfit = profit[i] - minPrice;
-            maxProfit = Math.max(potentialProfit , maxProfit);
+            if (buy > profit[i]) buy = profit[i];
+            else if (buy < profit[i]) maxProfit = Math.max(profit[i] - buy, maxProfit);
         }
         return maxProfit;
     }
